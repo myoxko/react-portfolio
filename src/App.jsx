ef275dc91925c6d96429fe18d0bf6985bca2abd5
myohwako <x.myohwa@gmail.com>
2025-11-10 12:30:48 +0900

    11/10_color

--- src/App.jsx
+++ src/App.jsx
@@ -33,3 +33,32 @@ function App() {
 }
 
 export default App
+
+
+/* import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'; // 라우터 관련 컴포넌트 임포트
+import Page1 from './Page1.jsx';
+import Page2 from './Page2.jsx';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <nav>
+        <Link to="/page1">페이지 1</Link> | {" "}
+        <Link to="/page2">페이지 2</Link>
+      </nav>
+      <Routes>
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
+ */
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/react-portfolio", //여기에 repository 경로를 추가
 })
